c920d5febeee559727a9e752ebaac8b9e11e7cae
tobyStupp <40300257+tobyStupp@users.noreply.github.com>
2021-02-07 03:21:42 +0200

    Loading this new project to practice stack data type

--- StackPractice1/Program.cs
+++ StackPractice1/Program.cs
@@ -1,15 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
+//using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Unit4;
 namespace StackPractice1
 {
     class Program
     {
+        static int Count (Stack<int> s)
+        {
+            //input : stack
+            //output: how many elements in stack without changing stack
+            throw new NotImplementedException();
+        }
+        static bool ValidExpression (string text)
+        {
+            //input: string of math expression (a+b) * c
+            // output true if parentesis is valid else false
+            throw new NotImplementedException();
+        }
+        static bool ValidExpressionWithDifferentParentesis(string text)
+        {
+            //input : string of math expression with different parentesis
+            //output: string is a valid expression
+            throw new NotImplementedException();
+        }
+        static bool Exists (Stack<int> stack, int x)
+        {
+            // input : stack and number 
+            // output : if number exists in stack. stack is not changed
+            throw new NotImplementedException();
+        }
+        static bool GetValueAt (Stack<int> stack, int place)
+        {
+            // input : stack and number
+            //output: value in stack at number place.
+            //assert there is a value at number place, and stack is not empty
+            throw new NotImplementedException();
+        }
+
         static void Main(string[] args)
         {
+            Stack<int> s = StackGenerator.GenerateRandomStack();
+            StackGenerator.PrintStack(s);
+            Console.WriteLine(Count(s));
+            StackGenerator.PrintStack(s);
+            string expression = "(a+b) * (c - d)";
+            if (ValidExpression(expression))
+                Console.WriteLine($"{expression} is valid");
+            else
+                Console.WriteLine($"{expression} is not valid");
+            expression = "[(a-b] * c/d)";
+            if (ValidExpressionWithDifferentParentesis(expression))
+                Console.WriteLine($"{expression} is valid");
+            else
+                Console.WriteLine($"{expression} is not valid");
+            Stack<int> st = StackGenerator.GenerateRandomStack(24);
+            StackGenerator.PrintStack(st);
+            Console.WriteLine("Get a number=>");
+            int number = int.Parse(Console.ReadLine());
+            if (Exists(st, number))
+                Console.WriteLine($"the value {number} is in the stack");
+            else
+                Console.WriteLine($"the value {number} is not in the stack");
+            Console.ReadKey();
+
         }
     }
 }
---
+++ StackPractice1/StackGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+//using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unit4;
+namespace StackPractice1
+{
+    class StackGenerator
+    {
+        public static Stack<int> GenerateRandomStack (int count = 25, int start = 1, int end = 50)
+        {
+            Random r = new Random();
+            Stack<int> temp = new Stack<int>();
+            for (int i =0; i < count; i++)
+            {
+                temp.Push(r.Next(start, end + 1));
+            }
+            return temp;
+        }
+        private static Stack<int> Clone (Stack<int> s)
+        {
+            Stack<int> temp = new Stack<int>();
+            Stack<int> cloned = new Stack<int>();
+            while (!s.IsEmpty())
+                temp.Push(s.Pop());
+            while (!temp.IsEmpty())
+            {
+                s.Push(temp.Top());
+                cloned.Push(temp.Pop());
+            }
+            return cloned;
+        }
+        public static void PrintStack(Stack<int> s)
+        {
+            Stack<int> temp = Clone(s);
+            Console.Write("[");
+            if (!temp.IsEmpty())
+                Console.Write(temp.Pop());
+            while (!temp.IsEmpty())
+            {
+                Console.Write($", {temp.Pop()}");
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
